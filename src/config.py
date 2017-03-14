cf18abcd0e76cb9ec9c0e1499244f182e419843e
iamtonny <pliomixxx@gmail.com>
2017-03-14 15:33:10 +0300

    Heroku test v6

--- src/config.py
+++ src/config.py
@@ -5,5 +5,5 @@ import os
 
 TOKEN = "375920397:AAGeEuNIDOpdpTPGDguWEFjUUitjriBZnNA"#os.environ['TOKEN']
 #DATABASE_URL = os.environ['DATABASE_URL']
-DATABASE_URL = "postgres://pwtuydtqnbmizv:1bac8724847dfbf8ffbf6770d03af5b2ab82491895045ab15341e4366e8b5197@ec2-54-243-185-99.compute-1.amazonaws.com:5432/d1q0i9bf8f7hg6"
+DATABASE_URL = "postgres://xdgvwvaibnwzdm:005a52deba1c3e17695f754263b18a4c0b3165d14fd2867ea8a9826abd35d049@ec2-54-243-55-1.compute-1.amazonaws.com:5432/dcnur2k5e3bipq"
 BASE_QUERY_URL = "https://query.yahooapis.com/v1/public/yql?"
