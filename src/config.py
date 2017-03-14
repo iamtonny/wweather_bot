79e47527ffdad5242591e99b4eba5aea34ccb40d
iamtonny <pliomixxx@gmail.com>
2017-03-14 15:46:49 +0300

    Changed token and db_url

--- src/config.py
+++ src/config.py
@@ -3,7 +3,6 @@
 
 import os
 
-TOKEN = "375920397:AAGeEuNIDOpdpTPGDguWEFjUUitjriBZnNA"#os.environ['TOKEN']
-#DATABASE_URL = os.environ['DATABASE_URL']
-DATABASE_URL = "postgres://xdgvwvaibnwzdm:005a52deba1c3e17695f754263b18a4c0b3165d14fd2867ea8a9826abd35d049@ec2-54-243-55-1.compute-1.amazonaws.com:5432/dcnur2k5e3bipq"
+TOKEN = os.environ['TOKEN']
+DATABASE_URL = os.environ['DATABASE_URL']
 BASE_QUERY_URL = "https://query.yahooapis.com/v1/public/yql?"
